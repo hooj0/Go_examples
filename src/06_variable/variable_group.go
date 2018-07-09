fb3d5c6e2eb9bff2cf78a06714fa61894f2f53ec
hoojo <hoojo@qq.com>
2018-07-09 21:31:13 +0800

    :sparkles: :bento: Added `variable group` sample

--- src/06_variable/variable_group.go
+++ src/06_variable/variable_group.go
@@ -17,16 +17,26 @@ variable group
 变量分组 同时声明多个类型变量，只需要一个var关键字
 变量分组 在变量很多的情况下使用,可读性好、结构清晰。
 变量分组 可以在func中或package中使用
+变量分组 可以在分组中设置变量类型或初始值，但不能两者同时都不设置
 ------------------------------------------------------------------*/
 
 package main
 
 import "fmt"
 
+// 声明一组变量
 var (
 	Data  string = "this is data str"
 	Index int    = 12
 	Flag  bool   = false
+
+	Cource float64	// 可以没有初始值
+)
+
+// 声明一组变量，可以没有变量类型
+var (
+	Failure = true
+	Max = 10
 )
 
 func main() {
@@ -39,4 +49,6 @@ func main() {
 
 	fmt.Println("Data: ", Data, ", Index: ", Index, ", Flag: ", Flag)
 	fmt.Println("a: ", a, ", x: ", x, ", z: ", z)
+
+	fmt.Println("Failure: ", Failure, ", Max: ", Max, ", Cource: ", Cource)
 }
