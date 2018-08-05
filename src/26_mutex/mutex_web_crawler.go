3f15d263d2d6200497d2a8b0a692a7e117f71af0
hoojo <hoojo@qq.com>
2018-08-05 18:42:22 +0800

    :sparkles: :bento: Added thread channel mutex `lock & unlock` web crawler sample

--- src/26_mutex/mutex_web_crawler.go
+++ src/26_mutex/mutex_web_crawler.go
@@ -21,7 +21,7 @@ package main
 import (
 	"fmt"
 	"sync"
-)
+	)
 
 
 type Cache struct {
@@ -34,39 +34,48 @@ type Fetcher interface {
 }
 
 // Crawl 使用 fetcher 从某个 URL 开始递归的爬取页面，直到达到最大深度。
-func Crawl(url string, depth int, fetcher Fetcher) {
+func Crawl(url string, depth int, fetcher Fetcher, resources chan string, quit chan string) {
 	// TODO: 并行的抓取 URL。
 	// TODO: 不重复抓取页面。
 
 	// 下面并没有实现上面两种情况：
 
-	go func() {
-		if depth <= 0 {
-			return
-		}
+	if depth <= 0 {
+		quit <- "q"
+		return
+	}
 
-		println("url: ", url, ", cache: ", c.caches[url])
-		if c.caches[url] == true {
-			return
-		}
+	// println("url: ", url, ", cache: ", c.caches[url])
+	if c.caches[url] == true {
+		// resources <-  "cache ->" + url
+		quit <- "q"
+		return
+	}
 
-		body, urls, err := fetcher.Fetch(url)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println("-->>", err)
+		quit <- "q"
+		return
+	}
 
-		mutex.Lock()
-		c.caches[url] = true
-		mutex.Unlock()
+	mutex.Lock()
+	c.caches[url] = true
+	mutex.Unlock()
 
-		fmt.Printf("found: %s %q\n", url, body)
+	resources <- fmt.Sprintf("found: %s %q", url, body)
 
-		for _, u := range urls {
-			Crawl(u, depth-1, fetcher)
-		}
-	}()
+	end := make(chan string)
+	for _, u := range urls {
+		go Crawl(u, depth-1, fetcher, resources, end)
+	}
 
+
+	for i := 0; i < len(urls); i++ {
+		<- end
+	}
+
+	quit <- "quit"
 	return
 }
 
@@ -74,7 +83,22 @@ var c = &Cache{ caches: make(map[string]bool) }
 var mutex = sync.Mutex{}
 
 func main() {
-	Crawl("https://golang.org/", 4, fetcher)
+
+	resources := make(chan string)
+	quit := make(chan string)
+
+	go Crawl("https://golang.org/", 4, fetcher, resources, quit)
+
+	for {
+
+		select {
+			case url := <- resources:
+				fmt.Println(url)
+			case <- quit:
+				return
+
+		}
+	}
 }
 
 // fakeFetcher 是返回若干结果的 Fetcher。
